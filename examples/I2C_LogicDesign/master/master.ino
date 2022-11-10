c825b0b6ac1d7afec8232c4fed76eabae7537f11
Aubrey Gatewood <gatewooa@purdue.edu>
2022-11-10 12:19:31 -0500

    integrated i2c functionality with code to read from CSV and implement acceleration moving average smoothing

--- examples/I2C_LogicDesign/master/master.ino
+++ examples/I2C_LogicDesign/master/master.ino
@@ -7,7 +7,29 @@ int state = 0; //0 = pre-liftoff, 1 = ascent, 2 = MECO, 3 = descent, 4 = post-de
 const int REMOTE = 8;
 const int LOCAL = 4;
 String StringData = "";
-//int size1 = 6;
+int newVar = 0;
+float pressThresh[4] = {85000, 0.1, .0008, .00175}; // (kpa) ground, MECO, coast-start, coast-end CALIBRATED ON HIGH END
+int accelThresh = 5; //m/s^2 UNCALIBRATED
+int lastState = 0;
+int pressFlag = 0;
+int accelFlag = 0;
+int timeFlag = 0;
+int stateFlag = 0; //0 = pre-liftoff, 1 = ascent, 2 = MECO, 3 = descent, 4 = post-descent
+int i = 0;
+int j = 0;
+
+float sum[6]; //1 - pressure new, 2 - pressure old, 3 - accel new, 4 - accel old, 5 - stdv accel new, 6 - stdv accel old
+float average[4]; //same indexing as sum
+
+float stdv1[30]; //1 - accel new, 2 - accel old
+float stdv2[30];
+float avstdv[2]; //1 - accel new, 2 - accel old
+
+float accelBuff1[30];
+float accelBuff2[30];
+float temp[2];
+
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(); // join I2C bus with address 4
@@ -25,31 +47,49 @@ void loop() {
   }
   int index = StringData.indexOf(",");
   String Pressure = StringData.substring(0,index);
-  Pressure.toFloat();
+  float pressure = Pressure.toFloat();
   Serial.print("pressure: ");
-  Serial.print(Pressure);
+  Serial.print(pressure);
   int indexEnd = StringData.indexOf(";");
   String Accel = StringData.substring(index+1, indexEnd);
-  Accel.toFloat();
+  float accel = Accel.toFloat();
   Serial.print(" accel: ");
-  Serial.println(Accel);
+  Serial.println(accel); // done printing raw data
+
+
+  float temp = accel;
+  float average[2] = {0,0}; // First is newer buffer, second is older
+  for (j = 29; j >= 1; j--)
+  {
+     accelBuff1[j] = accelBuff1[j - 1]; //Slide all the buffer 1 point to the right for newer buffer
+     accelBuff2[j] = accelBuff2[j - 1]; // Same as above but with older buffer
+  }
+  accelBuff1[0] = temp; // Set first point of new buffer to newly aquired accelerometer data
+  temp = accelBuff1[29];
+  accelBuff2[0] = temp; // Set first value of older buffer to last value of newer buffer (sliding)
+  for (j = 0; j < 30; j++)
+  {
+    average[0] += accelBuff1[j];
+    average[1] += accelBuff2[j];
+  }
+  average[0] /= 30;
+  average[1] /= 30;
+  Serial.println("Average 0: " + String(average[0]));
+  Serial.println("Average 1: " + String(average[1]));
+  if ((average[0] - average[1] >= accelThresh) && lastState != 1)
+  {
+    Serial.println("Entered Liftoff at index: " + String(i));
+    lastState = 1;
+  }
+  else if ((average[1] - average[0] >= accelThresh) && lastState != 2)
+  {
+    Serial.println("Entered MECO at index: " + String(i));
+    lastState = 2;
+  }  
+  
+  average[1] = 0;
+  average[2] = 0;
+  
   delay(1000);
   
 }
-
-
-
-/*
-void receiveEvent(int size1)
-{
-  Serial.println("receive event");
-  while (Wire.available()){
-    char c = Wire.read();
-    Serial.print(c);
-    //Serial.print("in while loop");
-  }
-  int x = Wire.read();
-  Serial.print(x);
-  Serial.println("is x value");
-}
-//*/
