2ebd68fb378b2e465bf1478543c45a9957b31667
vlazed <72566071+lavzed@users.noreply.github.com>
2023-09-21 15:02:18 -0400

    Tested delay of pin impulse without pausing execution

--- examples/pin_controller/pin_controller.ino
+++ examples/pin_controller/pin_controller.ino
@@ -2,21 +2,28 @@
 
 PinCtrl PinController(A1, 5, A2, 6, A3, 9, 13);
 
+unsigned long lastTime{0};
+
 void setup() {
     Serial.begin(9600);
     if (PinController.init() != 0) {
         Serial.println("Something went wrong with the PinController. Exiting...");
         exit(1);
     }
+    lastTime = micros();
 }
 
+/**
+ * @brief The difference in execution time should be below the set
+ * time delay of PinController impulse (50 ms). Typically, we achieved
+ * about 2 microseconds for the Feather M0.
+ * 
+ */
 void loop() {
-    delay(1000);
-    PinController.closeFlowValve1();
-    PinController.closeFlowValve2();
-    PinController.openVentValve();
+    Serial.println(micros() - lastTime);
     PinController.Run();
     PinController.openFlowValve1();
     PinController.openFlowValve2();
-    PinController.closeVentValve();
+    PinController.openVentValve();
+    lastTime = micros();
 }
--- examples/pin_controller/src/PinCtrl.cpp
+++ examples/pin_controller/src/PinCtrl.cpp
@@ -1,5 +1,22 @@
 #include "PinCtrl.h"
 
+const float t_delay{50};
+
+/**
+ * @brief Construct a PinController class with the pins corresponding to a
+ * valve or signal on the FEMTA Suborbital Experiment. The flow and vent
+ * pins correspond to valves on the Propellant Management Experiment, while
+ * the signal pin is an output which is measured by the FEMTA Thruster Experiment
+ * and determines whether the Thruster Experiment should begin at the right time.
+ * 
+ * @param flowClose1 
+ * @param flowOpen1 
+ * @param flowClose2 
+ * @param flowOpen2 
+ * @param ventClose 
+ * @param ventOpen 
+ * @param signal 
+ */
 PinCtrl::PinCtrl(
   int flowClose1,
   int flowOpen1,  
@@ -25,6 +42,13 @@ PinCtrl::PinCtrl(
   closeSV1_bool = 0;
 }
 
+
+/**
+ * @brief Initialize the pin modes and set the initial state of the valves
+ * at pre-flight.
+ * 
+ * @return int 
+ */
 int PinCtrl::init() {
     pinMode(closeSV1, OUTPUT);
     pinMode(closeSV2, OUTPUT);
@@ -40,43 +64,59 @@ int PinCtrl::init() {
     return 0;
 }
 
+/**
+ * @brief An overridden Protothread method which acts as a state machine that
+ * checks if a certain pin is turned on and sets the pin to HIGH. After a certain
+ * time delay t_delay, set the pin to LOW.
+ * 
+ * @return true 
+ * @return false 
+ */
 bool PinCtrl::Run() {
+  static float lastTime = 0;
   PT_BEGIN();
 
     for(;;) {
-      if (closeSV1_bool) {
+      lastTime = millis();
+       if (closeSV1_bool) {
         digitalWrite(closeSV1, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         closeSV1_bool = 0;
         digitalWrite(closeSV1, LOW);
       }
       if (closeSV2_bool) {
         digitalWrite(closeSV2, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         closeSV1_bool = 0;
         digitalWrite(closeSV2, LOW);
       }
       if (openSV1_bool) {
         digitalWrite(openSV1, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         closeSV1_bool = 0;
         digitalWrite(openSV1, LOW);
       }
       if (openSV2_bool) {
         digitalWrite(openSV2, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         closeSV1_bool = 0;
         digitalWrite(openSV2, LOW);
       }
       if (closeLV1_bool) {
         digitalWrite(closeLV1, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         closeLV1_bool = 0;
         digitalWrite(closeLV1, LOW);
       }
       if (openLV1_bool) {
         digitalWrite(openLV1, HIGH);
-        PT_YIELD();
+        PT_WAIT_UNTIL(millis() - lastTime > t_delay);
+        lastTime = millis();
         openLV1_bool = 0;
         digitalWrite(openLV1, LOW);
       }
@@ -86,39 +126,72 @@ bool PinCtrl::Run() {
   PT_END();
 }
 
+/**
+ * @brief Open flow valve 1
+ * 
+ */
 void PinCtrl::openFlowValve1() {
   if (!openSV1_bool && !closeSV1_bool)
     openSV1_bool = 1;
 }
 
+/**
+ * @brief Close flow valve 1
+ * 
+ */
 void PinCtrl::closeFlowValve1() {
   if (!closeSV1_bool && !openSV1_bool)
     closeSV1_bool = 1;
 }
 
+/**
+ * @brief Open flow valve 2
+ * 
+ */
 void PinCtrl::openFlowValve2() {
   if (!openSV2_bool && !closeSV2_bool)
     openSV2_bool = 1;
 }
 
+/**
+ * @brief Close flow valve 2
+ * 
+ */
 void PinCtrl::closeFlowValve2(){
   if (!closeSV2_bool && !openSV2_bool)
     closeSV2_bool = 1;
 }
 
+/**
+ * @brief Open vent valve 1
+ * 
+ */
 void PinCtrl::openVentValve(){
   if (!openLV1_bool && !openLV1_bool)
     openLV1_bool = 1;
 }
+
+/**
+ * @brief Close vent valve 1
+ * 
+ */
 void PinCtrl::closeVentValve() {
   if (!closeLV1_bool && !openLV1_bool)
     closeLV1_bool = 1;
 }
 
+/**
+ * @brief Signal the start of an experiment
+ * 
+ */
 void PinCtrl::signalStart(){
   digitalWrite(signalPin, HIGH);
 }
 
+/**
+ * @brief Signal to stop the experiment
+ * 
+ */
 void PinCtrl::signalStop(){
   digitalWrite(signalPin, LOW);
 }
